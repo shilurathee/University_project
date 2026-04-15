d9a5fba86561f5c6ac6c03ae9ad72219806308b5
shilurathee <ratheesushil225@gmail.com>
2026-04-15 09:57:14 +0530

    final commit
    final commit on 15 -04 with report and presnetation

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from db import get_db_connection
-from dropout_model import get_student_features
+
 
 
 app = Flask(__name__)
